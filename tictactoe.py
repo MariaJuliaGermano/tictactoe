080f76199a899d9c529b593b82e75a2bd7641b30
Maria Julia Germano <166447580+MariaJuliaGermano@users.noreply.github.com>
2024-09-19 17:47:13 -0300

    Final adjustments

--- tictactoe.py
+++ tictactoe.py
@@ -6,12 +6,11 @@ def set_tile(row, column):
     if (game_over):
         return
 
-    if board[row][column]["text"] != "":  # Verificando se o botão já está sendo marcado, para não ter problema de estar reescrevendo ele
+    if board[row][column]["text"] != "":  
         return 
 
-    board[row][column]['text'] = current_player # Marcando no board a jogada do jogador da vez
+    board[row][column]['text'] = current_player 
 
-    # Alternando a vez dos jogadores
     if current_player == playerO:
         current_player = playerX
     else:
@@ -25,16 +24,14 @@ def check_winner():
     global turns, game_over
     turns +=1
 
-    # Verificando vitória na horizontal, 3 linhas 
     for row in range(3):
         if (board[row][0]["text"] == board[row][1]["text"] == board[row][2]["text"] and board[row][0]["text"] != ""):
             label.config(text=board[row][0]["text"]+" is the winner!", foreground=color_green)
-            for column in range(3): # Pintando as marcações da vitória de cores diferentes para se destacar
+            for column in range(3):
                 board[row][column].config(foreground=color_green, background=color_light_gray)
             game_over = True
             return
         
-    # Verificando vitória na vertical, 3 na coluna
     for column in range(3):
         if (board[0][column]["text"] == board[1][column]["text"] == board[2][column]["text"] and board[0][column]["text"] != ""):
             label.config(text=board[0][column]["text"]+' is the winner!', foreground=color_green)
@@ -43,7 +40,6 @@ def check_winner():
             game_over = True
             return
 
-    # Verificando cada vitória na diagonal
     if (board[0][0]["text"] == board[1][1]["text"] == board[2][2]['text'] and board[0][0]["text"] != ""): 
         label.config(text=board[0][0]["text"]+' is the winner!', foreground=color_green)
         for i in range(3):
@@ -51,7 +47,6 @@ def check_winner():
         game_over = True
         return
     
-    # Verificando cada vitória na antidiagonal
     if (board[0][2]["text"] == board[1][1]["text"] == board[2][0]['text'] and board[0][2]["text"] != ""): 
         label.config(text=board[0][2]["text"]+' is the winner!', foreground=color_green)
         board[0][2].config(foreground=color_green, background=color_light_gray)
@@ -60,7 +55,6 @@ def check_winner():
         game_over = True
         return
     
-    # Verificando empate
     if turns == 9:
         game_over = True
         label.config(text="Empate!", background=color_gray, foreground=color_yellow)
@@ -78,7 +72,7 @@ def new_game():
 
 playerX = "X"
 playerO = "O"
-current_player = playerX # O jogador que está na vez
+current_player = playerX 
 board = [[0, 0, 0],
          [0, 0, 0],
          [0, 0, 0]]
@@ -94,14 +88,12 @@ game_over = False
 
 window = tkinter.Tk()
 window.title("Tic Tac Toe")
-window.resizable(False, False) # Não permitir que a janela seja redimensionada
+window.resizable(False, False) 
 
-# Criando o frame que atua como container
-frame = tkinter.Frame(window) # Colocando o frame dentro da janela
-# Criando componentes
+frame = tkinter.Frame(window) 
 label = tkinter.Label(frame, text=current_player+"'s turn", font=("Lato", 20), background=color_gray, foreground="white")
 
-label.grid(row=0, column=0, columnspan=3, sticky="we") # columnspan faz com que ele possa ocupar as 3 colunas e o sticky faz com que e componente vá do west p east, direta para esquerda ocupando todo o espaço
+label.grid(row=0, column=0, columnspan=3, sticky="we") 
 
 for row in range(3):
     for column in range(3):
@@ -115,14 +107,13 @@ button.grid(row=4, column=0, columnspan=3, sticky="we")
 
 frame.pack()
 
-# Fazendo a janela aparecer no centro da tela
-window.update() # Garantindo que todas as alterações no layout sejam aplicadas antes de calcular a posição da janela.
-window_width = window.winfo_width() # Largura da janela
-window_height = window.winfo_height() # Altura da janela
-screen_width = window.winfo_screenwidth() # Largura da tela
-screen_height = window.winfo_screenheight() # Altura da tela
+window.update() 
+window_width = window.winfo_width() 
+window_height = window.winfo_height() 
+screen_width = window.winfo_screenwidth() 
+screen_height = window.winfo_screenheight() 
 
-window_x = int((screen_width/2) - (window_width/2)) # Tirando metade da largura da janela d
+window_x = int((screen_width/2) - (window_width/2))
 window_y = int((screen_height/2) - (window_height/2))
 
 window.geometry(f"{window_width}x{window_height}+{window_x}+{window_y}")
